a38d28edf24c2b7eeaaa60bfa5a257706df893b3
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-11-12 23:27:47 +0000

    Add task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+a Flask web application
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ”, followed by the value of the text variable
+/python/(<text>): display “Python ”, followed by the value of the text variable
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Returns string to home page"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns string to /hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Returns string to /c/text"""
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/c/', defaults={'text': 'is cool'})
+@app.route('/c/(<text>)', strict_slashes=False)
+def p(text):
+    """Returns string to /c/text"""
+    return 'Python %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
